729cbb0f07d3077028c4e20062e5540a67fa19f6
Neider Parra <neiderpb24@gmail.com>
2024-09-05 14:35:07 -0500

    feat: Ajuste de login responsive y adelanto de tablas generales

---
+++ src/app/views/general-table/general-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './general-table.component.html',
+  styleUrl: './general-table.component.css'
+})
+export class GeneralTableComponent {
+
+}
