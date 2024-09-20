984a8cbca69b973e1da9dfe45843f3a3d1b3048a
Musse <mussebrahiam11@gmail.com>
2024-09-19 19:08:00 -0500

    feat/ ajuste de mostrar botones directamente en el componente

--- src/app/shared/group-button-general/group-button-general.component.ts
+++ src/app/shared/group-button-general/group-button-general.component.ts
@@ -7,12 +7,38 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class GroupButtonGeneralComponent {
   @Input() buttonText: string = 'Agregar';
-  @Input() showAddButton: boolean = false;
-  @Input() showImportButton: boolean = false;
-  @Input() showExportButton: boolean = false;
+  @Input() component: string = '';
+  public showAddButton: boolean = false;
+  public showImportButton: boolean = false;
+  public showExportButton: boolean = false;
   @Output() buttonClick = new EventEmitter<string>();
+
+  async ngOnInit(): Promise<void> {
+    this.checkPermissionsButton();
+  }
 
   onClick(action: string) {
     this.buttonClick.emit(action);
+  }
+
+  checkPermissionsButton() {
+    const permissions = JSON.parse(localStorage.getItem('permissions') || '[]');
+    for (const group of permissions) {
+      for (const module of group.modules) {
+        if (module.module_name === this.component) {
+          module.permissions.forEach((perm: any) => {
+            if (perm.name === 'Crear') {
+              this.showAddButton = true;
+            }
+            if (perm.name === 'Importar') {
+              this.showImportButton = true;
+            }
+            if (perm.name === 'Exportar') {
+              this.showExportButton = true;
+            }
+          });
+        }
+      }
+    }
   }
 }
