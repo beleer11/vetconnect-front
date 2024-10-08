d60d4ab614eb9821e6ba3fc26c9cb65846cd631c
Neider Parra <neiderpb24@gmail.com>
2024-10-07 21:36:12 -0500

    fix/Creación del componente de tipo de raza

--- src/app/services/parameter/type-breed/type-breed.service.ts
+++ src/app/services/parameter/type-breed/type-breed.service.ts
@@ -25,4 +25,49 @@ export class TypeBreedService {
 
   constructor(private http: HttpClient) { }
 
+  getDataTypesBreeds(params: any): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/type-breed/index`, {
+      params: params,
+      ...this.getHttpOptions(),
+    });
+  }
+
+  deleteRecordById(id: number): Observable<any> {
+    return this.http.delete(
+      `${this.apiUrl}/type-breed/remove/${id}`,
+      this.getHttpOptions()
+    );
+  }
+
+  disableRecordTypesBreedsById(id: number): Observable<any> {
+    return this.http.patch(
+      `${this.apiUrl}/type-breed/disable/${id}`,
+      {},
+      this.getHttpOptions()
+    );
+  }
+
+  enableRecordTypesBreedsById(id: number): Observable<any> {
+    return this.http.patch(
+      `${this.apiUrl}/type-breed/enable/${id}`,
+      {},
+      this.getHttpOptions()
+    );
+  }
+
+
+  sendTypesBreeds(data: any): Observable<any> {
+    return this.http.post<any>(
+      `${this.apiUrl}/type-breed/setData`,
+      data,
+      this.getHttpOptions()
+    );
+  }
+
+  editTypesBreeds(data: any, id: number): Observable<any> {
+    return this.http.put<any>(
+      `${this.apiUrl}/type-breed/editData/` + id, data,
+      this.getHttpOptions()
+    );
+  }
 }
